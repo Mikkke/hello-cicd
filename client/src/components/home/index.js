1cacfa5a77d4999d1e73e7cd42bf296907e4507b
Mikkke <jkellymichael@gmail.com>
2020-02-17 15:55:18 +0100

    changement de home

--- client/src/components/home/index.js
+++ client/src/components/home/index.js
@@ -1,21 +1,21 @@
 import React, { useState, useEffect } from 'react';
 
 const Home = () => {
-    const [message, setMessage] = useState("");
+  const [message, setMessage] = useState("");
 
-    useEffect(() => {
-      fetch("/api/home")
-        .then(res => res.text())
-        .then(res => setMessage(res));
-    }, []);
-  
-  
-    return (
-      <div>
-        <h1>Home</h1>
-        <p>{message}</p>
-      </div>
-    );
+  useEffect(() => {
+    fetch("/api/home")
+      .then(res => res.text())
+      .then(res => setMessage(res));
+  }, []);
+
+
+  return (
+    <div>
+      <h1>HOME</h1>
+      <p>{message}</p>
+    </div>
+  );
 }
 
 export default Home;
